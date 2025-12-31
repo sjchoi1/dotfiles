1710bf5744869a963eabeef7fece4c1da12d07c3
sjchoi <sjchoi@casys.kaist.ac.kr>
2025-12-31 06:24:47 +0000

    Hide close icons on buffer tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -117,7 +117,7 @@ require("lazy").setup({
       require("bufferline").setup({
         options = {
           mode = "buffers",
-          show_buffer_close_icons = true,
+          show_buffer_close_icons = false,
           show_close_icon = false,
         },
       })
